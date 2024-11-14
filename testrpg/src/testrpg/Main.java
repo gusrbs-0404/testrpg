635c3ccce6b64d2233074bb954952ce5cbc15d9b
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-14 14:58:59 +0900

    Add Unit,Stage,RPGGame, Player, Monster, Main class

---
+++ testrpg/src/testrpg/Main.java
@@ -0,0 +1,8 @@
+package testrpg;
+
+public class Main {
+	public static void main(String[] args) {
+		RPGGame game = RPGGame.getInstance();
+		game.run();
+	}
+}
---
+++ testrpg/src/testrpg/RPGGame.java
@@ -0,0 +1,16 @@
+package testrpg;
+
+public class RPGGame {
+	private void RPGGame() {
+	}
+
+	private static RPGGame instance = new RPGGame();
+
+	public static RPGGame getInstance() {
+		return instance;
+	}
+
+	public void run() {
+
+	}
+}
