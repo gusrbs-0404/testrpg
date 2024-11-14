f5e266cde342f76093c9575b2311020a06e6cf55
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-14 17:15:20 +0900

    Update Dragon 생성자

--- testrpg/src/testrpg/Dragon.java
+++ testrpg/src/testrpg/Dragon.java
@@ -1,5 +1,28 @@
 package testrpg;
 
-public class Dragon {
+public class Dragon extends Monster {
 
+	public Dragon(String name, int hp, int att, int def, int exp) {
+		super(Monster.getname("드래곤"), getHp(), getAtt(), getDef(), getExp());
+	}
+
+	private static int getHp() {
+		int hp = ran.nextInt(200) + 800;
+		return hp;
+	}
+
+	private static int getAtt() {
+		int def = ran.nextInt(50) + 80;
+		return def;
+	}
+
+	private static int getDef() {
+		int def = ran.nextInt(50) + 100;
+		return def;
+	}
+
+	private static int getExp() {
+		int def = ran.nextInt(30) + 10;
+		return def;
+	}
 }
