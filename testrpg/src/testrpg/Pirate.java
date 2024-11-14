b12b8dabd3161f0522f0dbaafb2c87d85b0d74c3
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-14 16:29:27 +0900

    Update 랜덤 hp,def 생성자 완성

--- testrpg/src/testrpg/Pirate.java
+++ testrpg/src/testrpg/Pirate.java
@@ -1,5 +1,18 @@
 package testrpg;
 
-public class Pirate {
+public class Pirate extends Player {
 
+	public Pirate(String name, int hp, int mp, int att, int def) {
+		super(Player.getname(), getHp(), 200, Player.getAtt(), getDef());
+	}
+
+	private static int getHp() {
+		int hp = ran.nextInt(400) + 300;
+		return hp;
+	}
+
+	private static int getDef() {
+		int def = ran.nextInt(50) + 30;
+		return def;
+	}
 }
