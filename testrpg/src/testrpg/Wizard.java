43b98bf59f33426b3c0ba16631fab3acab7dba8a
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-14 16:33:03 +0900

    Update Wizard 랜덤 hp, def 완성

--- testrpg/src/testrpg/Wizard.java
+++ testrpg/src/testrpg/Wizard.java
@@ -1,5 +1,19 @@
 package testrpg;
 
-public class Wizard {
+public class Wizard extends Player {
+
+	public Wizard(String name, int hp, int mp, int att, int def) {
+		super(Player.getname(), getHp(), 300, Player.getAtt(), getDef());
+	}
+
+	private static int getHp() {
+		int hp = ran.nextInt(100) + 200;
+		return hp;
+	}
+
+	private static int getDef() {
+		int def = ran.nextInt(30) + 10;
+		return def;
+	}
 
 }
