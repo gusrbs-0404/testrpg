af2d5ee767c9c017a995f4d255dd8e07f5d2e722
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-14 16:56:59 +0900

    Update Monset 생성자, Style Playre 정렬

--- testrpg/src/testrpg/Monster.java
+++ testrpg/src/testrpg/Monster.java
@@ -1,5 +1,19 @@
 package testrpg;
 
-public class Monster {
+public abstract class Monster {
+	protected String name;
+	protected int MAX_HP;
+	protected int hp;
+	protected int att;
+	protected int def;
+	protected int exp;
 
+	public Monster(String name, int hp, int att, int def, int exp) {
+		this.name = name;
+		this.hp = hp;
+		this.att = att;
+		this.def = def;
+	}
+	
+	
 }
