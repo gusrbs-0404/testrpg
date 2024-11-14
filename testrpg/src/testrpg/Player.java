a4961dc8dca38f9f6f35d6065a5442f8610a59d3
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-14 15:59:09 +0900

    Update Playre class / money, skull()

--- testrpg/src/testrpg/Player.java
+++ testrpg/src/testrpg/Player.java
@@ -1,5 +1,14 @@
 package testrpg;
 
-public class Player {
+public class Player extends Unit {
 
+	public int money;
+
+	public Player(String name, int hp, int mp, int att, int def) {
+		super(name, hp, mp, att, def);
+	}
+
+	void skill() {
+
+	}
 }
