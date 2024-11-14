5c32f02adcce253d7d12aede473d819cd4785a50
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-14 16:24:35 +0900

    Update Archer 랜덤hp,def 생성자 완성

--- testrpg/src/testrpg/Archer.java
+++ testrpg/src/testrpg/Archer.java
@@ -1,5 +1,18 @@
 package testrpg;
 
-public class Archer {
+public class Archer extends Player {
 
+	public Archer(String name, int hp, int mp, int att, int def) {
+		super(Player.getname(), getHp(), 150, Player.getAtt(), getDef());
+	}
+
+	private static int getHp() {
+		int hp = ran.nextInt(300) + 100;
+		return hp;
+	}
+
+	private static int getDef() {
+		int def = ran.nextInt(50) + 30;
+		return def;
+	}
 }
