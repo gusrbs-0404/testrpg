19a1274e1fcfc0f26544768a7eba7d13e9b2f566
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-14 16:27:58 +0900

    Update Bandit 랜덤 hp,def 생성자 완성

--- testrpg/src/testrpg/Bandit.java
+++ testrpg/src/testrpg/Bandit.java
@@ -1,5 +1,19 @@
 package testrpg;
 
-public class Bandit {
+public class Bandit extends Player {
+
+	public Bandit(String name, int hp, int mp, int att, int def) {
+		super(Player.getname(), getHp(), 200, Player.getAtt(), getDef());
+	}
+
+	private static int getHp() {
+		int hp = ran.nextInt(300) + 200;
+		return hp;
+	}
+
+	private static int getDef() {
+		int def = ran.nextInt(40) + 30;
+		return def;
+	}
 
 }
