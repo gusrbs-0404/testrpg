846e6965f27429eeb5bb19a9f7727e87429f345f
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-14 16:49:56 +0900

    Update Item 생성자

--- testrpg/src/testrpg/Item.java
+++ testrpg/src/testrpg/Item.java
@@ -5,9 +5,17 @@ public class Item {
 	final int ARMOR = 2;
 	final int RING = 3;
 
-	int kind;
-	String name;
-	int plusAtt;
-	int plusdet;
-	int price;
+	int kind; // 종류
+	String name; // 이름
+	int plusAtt; // 공격력
+	int plusdet; // 방어력
+	int price; // 가격
+
+	public void setItem(int kind, String name, int plusAtt, int plusdet, int price) {
+		this.kind = kind;
+		this.name = name;
+		this.plusAtt = plusAtt;
+		this.plusdet = plusdet;
+		this.price = price;
+	}
 }
