dafd292abdb380d65da504e1db2e12ef1bfe4947
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-14 17:07:46 +0900

    Update Monster 랜덤 이름

--- testrpg/src/testrpg/Monster.java
+++ testrpg/src/testrpg/Monster.java
@@ -1,5 +1,7 @@
 package testrpg;
 
+import java.util.Random;
+
 public abstract class Monster {
 	protected String name;
 	protected int MAX_HP;
@@ -8,6 +10,8 @@ public abstract class Monster {
 	protected int def;
 	protected int exp;
 
+	public static Random ran = new Random();
+
 	public Monster(String name, int hp, int att, int def, int exp) {
 		this.name = name;
 		this.hp = hp;
@@ -15,4 +19,11 @@ public abstract class Monster {
 		this.def = def;
 	}
 
+	public static String getname(String title) {
+		String[] getName = { "강력한", "활기찬", "굶주린", "인간 사냥꾼", "겁쟁이", "똑똑한", "현명한", "독", "화염", "얼음", "멍청한" };
+		int nameSize = ran.nextInt(getName.length);
+
+		String name = getName + " " + title;
+		return name;
+	}
 }
