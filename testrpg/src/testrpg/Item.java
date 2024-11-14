4b854f8063f27f152ed7940d7b0b347593ff43b0
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-14 15:43:14 +0900

    Add Item class

---
+++ testrpg/src/testrpg/Item.java
@@ -0,0 +1,13 @@
+package testrpg;
+
+public class Item {
+	final int WEAPON = 1;
+	final int ARMOR = 2;
+	final int RING = 3;
+
+	int kind;
+	String name;
+	int plusAtt;
+	int plusdet;
+	int price;
+}
