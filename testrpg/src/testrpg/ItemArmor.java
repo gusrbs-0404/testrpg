cfd3dd9c59aedca8994fbde8eed4e7db11db1acb
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-15 17:17:38 +0900

    Update ItemArmor class 랜덤 갑옷 생성자

--- testrpg/src/testrpg/ItemArmor.java
+++ testrpg/src/testrpg/ItemArmor.java
@@ -2,4 +2,16 @@ package testrpg;
 
 public class ItemArmor extends Item {
 
+	public ItemArmor() {
+		super(ARMOR, ranName(), Item.plusAttAnddet(), Item.plusAttAnddet(), Item.price());
+	}
+
+	public String Name() {
+		String[] weapon = { "모자", "상의", "바지", "신발" };
+
+		int ranIndex = ran.nextInt(weapon.length);
+
+		String name = Item.ranName() + " " + weapon[ranIndex];
+		return name;
+	}
 }
