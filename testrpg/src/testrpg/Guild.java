8d60c50e1ffc7e36ef0a767fd3d1ee0475713537
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-15 15:17:49 +0900

    Update Guild class deport() 근데 종료하고 다시 들어오면 길드원 초기화됨

--- testrpg/src/testrpg/Guild.java
+++ testrpg/src/testrpg/Guild.java
@@ -44,7 +44,7 @@ public class Guild extends Stage implements Runnable {
 	private void inquiry() {
 		// 길드원 조회 최대 10명
 		// 전부 출력
-		if(player.size() == 0) {
+		if (player.size() == 0) {
 			System.out.println("길드원이 없습니다.");
 			return;
 		}
@@ -52,7 +52,9 @@ public class Guild extends Stage implements Runnable {
 	}
 
 	private void printPlayer() {
+		int count = 1;
 		for (Player i : player) {
+			System.out.print(count++ + "번 | ");
 			System.out.println(i);
 		}
 		System.out.printf("총인원 수 : %d\n", player.size());
@@ -96,6 +98,28 @@ public class Guild extends Stage implements Runnable {
 		// 길드원 출력
 		printPlayer();
 		// 선택한 길드원 이름 으로 추방
-		
+		try {
+			System.out.print("추방할 길드원 번호 입력 : ");
+			buffer.setLength(0);
+			String number = reader.readLine();
+			inputIndex(number);
+		} catch (Exception e) {
+		}
+	}
+
+	private void inputIndex(String number) {
+		int num = -1;
+		try {
+			num = Integer.parseInt(number) - 1;
+			if (num < 0 || num >= player.size()) {
+				System.err.println("잘못입력했습니다.");
+			}
+		} catch (Exception e) {
+			System.err.println("잘못입력했습니다.");
+			return;
+		}
+
+		System.out.printf("%s 를 추방합니다.\n", player.get(num));
+		player.remove(num);
 	}
 }
