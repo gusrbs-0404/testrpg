86903bc41d2b335f4a28e751fe34052dc21617ac
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-15 14:29:27 +0900

    Update ranUnit() 랜덤으로 영입

--- testrpg/src/testrpg/Guild.java
+++ testrpg/src/testrpg/Guild.java
@@ -2,18 +2,26 @@ package testrpg;
 
 public class Guild extends Stage implements Runnable {
 
+	private final int ARCHER = 1;
+	private final int WARRIOR = 2;
+	private final int WIZARD = 3;
+	private final int BANDIT = 4;
+	private final int PIRATE = 5;
+
 	private boolean isGuild = true;
 
 	@Override
 	public void run() {
-		try {
-			System.out.println("=====[GUILD]=====");
-			System.out.println("[1. 조회] [2. 영입] [3. 추방] [4. 종료]");
-			buffer.setLength(0);
-			System.out.print("메뉴 입력 : ");
-			String select = reader.readLine();
-			menu(select);
-		} catch (Exception e) {
+		while (isGuild) {
+			try {
+				System.out.println("=====[GUILD]=====");
+				System.out.println("[1. 조회] [2. 영입] [3. 추방] [4. 종료]");
+				buffer.setLength(0);
+				System.out.print("메뉴 입력 : ");
+				String select = reader.readLine();
+				menu(select);
+			} catch (Exception e) {
+			}
 		}
 	}
 
@@ -36,18 +44,36 @@ public class Guild extends Stage implements Runnable {
 	private void inquiry() {
 		// 길드원 조회 최대 10명
 		// 전부 출력
-
+		
 	}
 
 	private void recruit() {
 		// 영입
-		// 새로운 길드원 랜덤으로 5명 표시
-		// 원하는 길드원 이름 입력받아서 대려오기
+		// 랜덤으로 아무나 대리고옴
+		ranUnit();
+		System.out.println(player);
 
 	}
 
 	private void ranUnit() {
-		int r = ran.nextInt();
+		int ranNum = ran.nextInt(5) + 1;
+		System.out.println(ranNum);
+		if (ranNum == ARCHER) {
+			Archer archer = new Archer();
+			player.add(archer);
+		} else if (ranNum == WARRIOR) {
+			Warrior warrior = new Warrior();
+			player.add(warrior);
+		} else if (ranNum == WIZARD) {
+			Wizard wizard = new Wizard();
+			player.add(wizard);
+		} else if (ranNum == BANDIT) {
+			Bandit bandit = new Bandit();
+			player.add(bandit);
+		} else if (ranNum == PIRATE) {
+			Pirate pirate = new Pirate();
+			player.add(pirate);
+		}
 	}
 
 	private void deport() {
