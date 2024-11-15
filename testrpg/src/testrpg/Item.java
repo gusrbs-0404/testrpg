9512c234fcc5501e5263dce0cddc6788bbf4d5cf
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-15 17:15:14 +0900

    Update Item class 랜덤 att, det, price method

--- testrpg/src/testrpg/Item.java
+++ testrpg/src/testrpg/Item.java
@@ -1,9 +1,11 @@
 package testrpg;
 
+import java.util.Random;
+
 public class Item {
-	final int WEAPON = 1;
-	final int ARMOR = 2;
-	final int RING = 3;
+	static final int WEAPON = 1;
+	static final int ARMOR = 2;
+	static final int RING = 3;
 
 	int kind; // 종류
 	String name; // 이름
@@ -11,11 +13,29 @@ public class Item {
 	int plusdet; // 방어력
 	int price; // 가격
 
-	public void setItem(int kind, String name, int plusAtt, int plusdet, int price) {
+	public static Random ran = new Random();
+
+	public Item(int kind, String name, int plusAtt, int plusdet, int price) {
 		this.kind = kind;
 		this.name = name;
 		this.plusAtt = plusAtt;
 		this.plusdet = plusdet;
 		this.price = price;
+	}
+
+	public static String ranName() {
+		String[] kind = { "나무", "철", "금", "다이아몬드" };
+		int kindIndex = ran.nextInt(kind.length);
+		return kind[kindIndex];
+	}
+
+	public static int plusAttAnddet() {
+		int ranAttDet = ran.nextInt(90) + 10;
+		return ranAttDet;
+	}
+
+	public static int price() {
+		int ranPrice = ran.nextInt(2400) + 500;
+		return ranPrice;
 	}
 }
