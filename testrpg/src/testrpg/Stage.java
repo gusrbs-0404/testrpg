62182284d1a7bf9a19fe6b066cc682738ce47bce
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-15 10:19:20 +0900

    Update Stage BufferedReader

--- testrpg/src/testrpg/Stage.java
+++ testrpg/src/testrpg/Stage.java
@@ -1,11 +1,15 @@
 package testrpg;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-interface Stage {
+interface Stage {	
 	// 문자열 입력 - 임시정장
 	public StringBuffer buffer = new StringBuffer();
 	// 문자열 출력 = print랑 비슷
 	public BufferedWriter wirter = new BufferedWriter(new OutputStreamWriter(System.out));
+	// 입력 받기
+	public BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 }
