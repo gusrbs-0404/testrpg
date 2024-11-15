48be1182d552ded3c1b90aa450a3f9eca718625a
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-15 11:36:59 +0900

    Add zombi, oak, dragon 생성만 체크

--- testrpg/src/testrpg/Battle.java
+++ testrpg/src/testrpg/Battle.java
@@ -1,11 +1,54 @@
 package testrpg;
 
 public class Battle implements Stage, Runnable {
+	private boolean isBattle = true;
 
 	@Override
 	public void run() {
-		System.out.println("=====[BATTLE]=====");
-		System.out.println("[1. 좀비] [2. 오크(LV3↑)] [3. 드래곤(LV5↑)] [4. 종료]");
+		isBattle = true;
+		while (isBattle) {
+			try {
+				System.out.println("=====[BATTLE]=====");
+				System.out.println("[1. 좀비] [2. 오크(LV3↑)] [3. 드래곤(LV5↑)] [4. 종료]");
+				buffer.setLength(0);
+				System.out.print("메뉴 입력 : ");
+				String select = reader.readLine();
+				menu(select);
+			} catch (Exception e) {
+
+			}
+		}
+	}
+
+	private void menu(String select) {
+		if (select.equals("좀비")) {
+			zombie();
+		} else if (select.equals("오크")) {
+			oak();
+		} else if (select.equals("드래곤")) {
+			dragon();
+		} else if (select.equals("종료")) {
+			System.out.println("전투 종료합니다.");
+			Thread lobbyThread = new Thread(Lobby.lobby);
+			isBattle = false;
+			lobbyThread.start();
+		}
+
+	}
+
+	private void zombie() {
+		Zombie zombie = new Zombie();
+		System.out.println(zombie);
+	}
+
+	private void oak() {
+		Oak oak = new Oak();
+		System.out.println(oak);
+	}
+
+	private void dragon() {
+		Dragon dragon = new Dragon();
+		System.out.println(dragon);
 	}
 
 }
