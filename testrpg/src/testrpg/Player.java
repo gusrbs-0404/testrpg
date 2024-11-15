9157131fe7d7eceb966aa8d75313bfa71fd2a4c3
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-15 14:56:17 +0900

    Style Playre class toString 출력문

--- testrpg/src/testrpg/Player.java
+++ testrpg/src/testrpg/Player.java
@@ -10,7 +10,7 @@ public class Player extends Unit {
 		super(name, hp, mp, att, def);
 	}
 
-	public static String getname() {
+	public static String getname(String title) {
 		String[] name1 = { "나약한", "배고픈", "강력한", "무기력한", "다재다능한", "다재무능한", "현란한", "묵직한", "장난쓰러운", "왕족", "귀족", "노비", "평민",
 				"재빠른" };
 		String[] name2 = { "길동이", "둘리", "희동이", "짱구", "도라애몽", "노진구", "퉁퉁이", "훈이", "맹구", "철수", "유리", "비실이", "이슬이", "흰둥이",
@@ -19,7 +19,7 @@ public class Player extends Unit {
 		int name1Size = ran.nextInt(name1.length);
 		int name2Size = ran.nextInt(name2.length);
 
-		String name = name1[name1Size] + " " + name2[name2Size];
+		String name = name1[name1Size] + " " + name2[name2Size] + " " + title;
 		return name;
 	}
 
@@ -30,7 +30,8 @@ public class Player extends Unit {
 
 	@Override
 	public String toString() {
-		String msg = String.format("%s | [%d/%d | %d/%d] | [%d/%d]", name, hp, MAX_HP, mp, MAX_MP, exp, MAX_EXP);
+		String msg = String.format("%s | [체력 %d/%d] | [마나 %d/%d]\n\t[공격력 %d] | [방어력 %d] | [경험치 %d/%d] ", name, hp,
+				MAX_HP, mp, MAX_MP, att, def, exp, MAX_EXP);
 		return msg;
 	}
 
