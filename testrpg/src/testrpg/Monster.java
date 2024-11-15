73eb14d0f46c308f76d834f295d32d020882c6d4
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-15 11:34:30 +0900

    Update name 속성표시, toString Override, MAX_HP 추가

--- testrpg/src/testrpg/Monster.java
+++ testrpg/src/testrpg/Monster.java
@@ -14,16 +14,24 @@ public abstract class Monster {
 
 	public Monster(String name, int hp, int att, int def, int exp) {
 		this.name = name;
+		this.MAX_HP = hp;
 		this.hp = hp;
 		this.att = att;
 		this.def = def;
 	}
 
 	public static String getname(String title) {
-		String[] getName = { "강력한", "활기찬", "굶주린", "인간 사냥꾼", "겁쟁이", "똑똑한", "현명한", "독", "화염", "얼음", "멍청한" };
+		String[] getName = { "강력한", "독", "화염", "얼음", "나무", "바다"};
 		int nameSize = ran.nextInt(getName.length);
 
-		String name = getName + " " + title;
+		String name = getName[nameSize] + " " + title;
+		System.out.println(name);
 		return name;
+	}
+
+	@Override
+	public String toString() {
+		String msg = String.format("[%s] | [%d/%d]\n [공격력 : %d | 방어력 : %d]", name, hp, MAX_HP, att, def);
+		return msg;
 	}
 }
