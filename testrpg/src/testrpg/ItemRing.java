3ecceb02dd6d2b9a94dd4800ffcdea75ecd95c7f
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-15 17:19:10 +0900

    Update ItemRing class 랜덤 장신구 생성자

--- testrpg/src/testrpg/ItemRing.java
+++ testrpg/src/testrpg/ItemRing.java
@@ -2,4 +2,17 @@ package testrpg;
 
 public class ItemRing extends Item {
 
+	public ItemRing() {
+		super(RING, ranName(), Item.plusAttAnddet(), Item.plusAttAnddet(), Item.price());
+	}
+
+	public String Name() {
+		String[] weapon = { "반지", "목걸이", "팔찌", "귀걸이", "벨트" };
+
+		int ranIndex = ran.nextInt(weapon.length);
+
+		String name = Item.ranName() + " " + weapon[ranIndex];
+		return name;
+	}
+
 }
