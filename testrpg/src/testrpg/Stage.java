1c0d6d9ca433596b495286a24acce831a925e2b6
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-15 14:05:05 +0900

    Add Stage class random 추가

--- testrpg/src/testrpg/Stage.java
+++ testrpg/src/testrpg/Stage.java
@@ -4,8 +4,11 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Random;
 
 interface Stage {	
+
+	public static Random ran = new Random();
 	// 문자열 입력 - 임시정장
 	public StringBuffer buffer = new StringBuffer();
 	// 문자열 출력 = print랑 비슷
