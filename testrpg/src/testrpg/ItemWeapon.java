006516c97b7a1b963103efcfc4461ede6678d094
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-15 17:15:30 +0900

    Update ItemWeapon class 랜덤 무기 생성자

--- testrpg/src/testrpg/ItemWeapon.java
+++ testrpg/src/testrpg/ItemWeapon.java
@@ -1,5 +1,17 @@
 package testrpg;
 
-public class ItemWeapon extends Item{
+public class ItemWeapon extends Item {
 
+	public ItemWeapon() {
+		super(WEAPON, ranName(), Item.plusAttAnddet(), Item.plusAttAnddet(), Item.price());
+	}
+
+	public String Name() {
+		String[] weapon = { "검", "활", "완드", "단검", "아대", "너클", "총" };
+
+		int ranIndex = ran.nextInt(weapon.length);
+
+		String name = Item.ranName() + " " + weapon[ranIndex];
+		return name;
+	}
 }
