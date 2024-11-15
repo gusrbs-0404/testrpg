e9f61284a16a9a19e3e5b1b8767df7f7b7d423fd
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-15 16:30:16 +0900

    Update Guilde class 추방 예외

--- testrpg/src/testrpg/Guild.java
+++ testrpg/src/testrpg/Guild.java
@@ -33,7 +33,7 @@ public class Guild extends Stage implements Runnable {
 		} else if (select.equals("추방")) {
 			deport();
 		} else if (select.equals("종료")) {
-			System.out.println("전투 종료합니다.");
+			System.out.println("길드메뉴룰 종료합니다.");
 			Thread lobbyThread = new Thread(Lobby.lobby);
 			isGuild = false;
 			lobbyThread.start();
@@ -45,13 +45,13 @@ public class Guild extends Stage implements Runnable {
 		// 길드원 조회 최대 10명
 		// 전부 출력
 		if (RPGGame.player.size() == 0) {
-			System.out.println("길드원이 없습니다.");
+			System.err.println("길드원이 없습니다.");
 			return;
 		}
 		printPlayer();
 	}
 
-	public void printPlayer() {
+	private void printPlayer() {
 		int count = 1;
 		for (Player i : RPGGame.player) {
 			System.out.print(count++ + "번 | ");
@@ -64,7 +64,7 @@ public class Guild extends Stage implements Runnable {
 		// 영입
 		// 랜덤으로 아무나 대리고옴
 		if (RPGGame.player.size() == 10) {
-			System.out.println("최대인원 입니다!");
+			System.err.println("최대인원 입니다!");
 			return;
 		}
 
@@ -95,6 +95,10 @@ public class Guild extends Stage implements Runnable {
 
 	private void deport() {
 		// 추방
+		if (RPGGame.player.size() == 0) {
+			System.err.println("길드원이 없습니다.");
+			return;
+		}
 		// 길드원 출력
 		printPlayer();
 		// 선택한 길드원 이름 으로 추방
@@ -119,43 +123,7 @@ public class Guild extends Stage implements Runnable {
 			return;
 		}
 
-		System.out.printf("%s 를 추방합니다.\n", RPGGame.player.get(num));
+		System.out.printf("%s\n 를 추방합니다.\n", RPGGame.player.get(num));
 		RPGGame.player.remove(num);
 	}
-
-	private void party() {
-		if (RPGGame.party.size() == 4) {
-			System.out.println("파티원 모집이 끝났습니다.");
-			return;
-		}
-
-		printPlayer();
-		try {
-			System.out.print("파티 모집할 길드원 번호 입력 : ");
-			buffer.setLength(0);
-			String number = reader.readLine();
-			inputParty(number);
-		} catch (Exception e) {
-		}
-
-	}
-
-	private void inputParty(String number) {
-		printPlayer();
-
-		int num = -1;
-		try {
-			num = Integer.parseInt(number) - 1;
-			if (num < 0 || num >= RPGGame.player.size()) {
-				System.err.println("잘못입력했습니다.");
-			}
-		} catch (Exception e) {
-			System.err.println("잘못입력했습니다.");
-			return;
-		}
-
-		System.out.printf("%s 를 파티모집 합니다.\n", RPGGame.party.get(num));
-		RPGGame.party.add(RPGGame.party.get(num));
-	}
-
 }
