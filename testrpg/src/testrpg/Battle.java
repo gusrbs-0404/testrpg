ee2b087afca5c332bfeaaff1be8ca1baa883ba34
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-17 19:46:20 +0900

    Add action() method - 랜덤 공격(공격 치명타 스킬), actionplayerIndex() method -

--- testrpg/src/testrpg/Battle.java
+++ testrpg/src/testrpg/Battle.java
@@ -1,7 +1,16 @@
 package testrpg;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Battle extends Stage implements Runnable {
 	private boolean isBattle = true;
+
+	private Zombie zombie;
+	private Oak oak;
+	private Dragon dragon;
+
+	private Random ran = new Random();
 
 	@Override
 	public void run() {
@@ -37,22 +46,74 @@ public class Battle extends Stage implements Runnable {
 	}
 
 	private void zombie() {
-		Zombie zombie = new Zombie();
-		System.out.println(zombie);
+		zombie = new Zombie();
+
+		while (true) {
+			int action = action();
+
+			switch (action) {
+			case 0:
+				damage = member.attack();
+				System.out.println(member.getName() + "이(가) 일반 공격! 데미지: " + damage);
+				break;
+			case 1:
+				damage = member.criticalHit();
+				System.out.println(member.getName() + "이(가) 치명타 공격! 데미지: " + damage);
+				break;
+			case 2:
+				damage = member.skillAttack();
+				System.out.println(member.getName() + "이(가) 스킬 공격! 데미지: " + damage);
+				break;
+			default:
+				damage = 0;
+				break;
+			}
+		}
 	}
 
 	private void oak() {
-		Oak oak = new Oak();
+		oak = new Oak();
 		System.out.println(oak);
 	}
 
 	private void dragon() {
-		Dragon dragon = new Dragon();
+		dragon = new Dragon();
 		System.out.println(dragon);
 		int a = dragon.attack(dragon);
 		System.out.println(a);
 	}
-	
-	
+
+	private boolean isHpMonster(Monster monster, Unit unit) {
+		if (monster.hp < 0) {
+			monster.hp = 0;
+			return false;
+		}
+		return true;
+	}
+
+	private boolean isHpUnit(ArrayList<Player> party) {
+		int count = 0;
+		for (int i = 0; i < RPGGame.party.size(); i++) {
+			if (party.get(i).hp < 0) {
+				party.get(i).hp = 0;
+				count++;
+			}
+
+			if (count == 4) {
+				return false;
+			}
+		}
+	}
+
+	private int action() {
+		int action = ran.nextInt(3);
+		// 0 공격 1 치명타 2 스킬
+		return action;
+	}
+
+	private int actionplayerIndex() {
+		int playerIndex = ran.nextInt(4);
+		return playerIndex;
+	}
 
 }
